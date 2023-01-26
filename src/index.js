9310467273c5b714840b28d304def9d7f07d9b7c
InnaShp <107990414+InnaShp@users.noreply.github.com>
2023-01-26 14:15:34 +0200

    Removed the cyclical dependence between state.js and index.js.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-import state from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import state, { subscribe } from './redux/state';
+import {addMessage, addPost, updateNewMessageText, updateNewPostText} from './redux/state';
+import { BrowserRouter } from "react-router-dom";
 
-import { rerenderEntireTree } from './render';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let rerenderEntireTree = (state) => {
+  
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} addMessage={addMessage} updateNewMessageText={updateNewMessageText} />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
+
+
+reportWebVitals();
 
 rerenderEntireTree(state);
+subscribe(rerenderEntireTree);
