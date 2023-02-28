81b592f3afbb9cbad04c0525a4c0b1aba433f22e
InnaShp <107990414+InnaShp@users.noreply.github.com>
2023-02-28 13:57:28 +0200

    Changed Users to class component; used componentDidMount() method to made ajax request

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,49 +3,50 @@ import React from 'react';
 import s from "./Users.module.css";
 import userPhoto from '../../assets/images/user.png'
 
-
-let Users = (props) => {
-  if (props.users.length === 0) {
+class Users extends React.Component {
+  componentDidMount() {
     axios.get("https://social-network.samuraijs.com/api/1.0/users")
     .then(response => {
-      props.setUsers(response.data.items);
+      this.props.setUsers(response.data.items);
     })
-    
   }
-  
-return <div>
-  {
-    props.users.map(u => <div key={u.id}>
-      <span>
-        <div><img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" className={s.userPhoto} /></div>
-        <div>
-          {u.followed 
-          ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button> 
-          : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
-          
-        </div>
-      </span>
-      <span>
-        <span>
-          <div>
-            {u.name}
-          </div>
-          <div>
-            {u.status}
-          </div>
-        </span>
-        <span>
-          <div>
-            
-          </div>
-          <div>
-            
-          </div>
-        </span>
-      </span>
-    </div>)
+ 
+  render() {
+    return <div> 
+      {
+        this.props.users.map(u => <div key={u.id}>
+          <span>
+            <div><img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" className={s.userPhoto} /></div>
+            <div>
+              {u.followed 
+              ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button> 
+              : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
+              
+            </div>
+          </span>
+          <span>
+            <span>
+              <div>
+                {u.name}
+              </div>
+              <div>
+                {u.status}
+              </div>
+            </span>
+            <span>
+              <div>
+                
+              </div>
+              <div>
+      
+              </div>
+            </span>
+          </span>
+        </div>)
+      }
+    </div>
   }
-</div>
 }
+
 
 export default Users;
